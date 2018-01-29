51955ed14a1b09e44667284790d82581bdbc592f
RAnderso0325 <reillyjanderson@gmail.com>
2018-01-29 14:44:22 -0800

    stubbed out initial components

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import './css/Main.css';
+import Month from './calendar/Month.js';
+import Week from './calendar/Week.js';
+import Day from './calendar/Day.js';
 
 class Main extends Component {
   render(){
     return (
-        <p>Home Page</p>
+        <div className="main-page">
+          <Month />
+          <Week />
+          <Day />
+        </div>
       );
   }
 }
