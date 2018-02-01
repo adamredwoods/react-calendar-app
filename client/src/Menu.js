ce62e643f81bde39c6ea9dfe461e1127db11ff42
adamredwoods <adamredwoods@users.noreply.github.com>
2018-01-31 17:57:28 -0800

    added menu

--- client/src/Menu.js
+++ client/src/Menu.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { Row, Col } from 'react-grid-system';
 import Holidays from 'date-holidays';
 import CountryCodes from './helper/CountryCodes.js';
+import "./css/Menu.css";
 const hd = new Holidays();
 
 class Menu extends Component {
@@ -11,7 +12,9 @@ class Menu extends Component {
         this.state = {
             countryCode: 'US',
             currentYear: '2018',
-            currentYearHolidays: []
+            currentYearHolidays: [],
+            menuClass: "menu-page",
+            menuToggle: 0
         }
         this.handleCountryCodeChange = this.handleCountryCodeChange.bind(this);
     }
@@ -43,10 +46,21 @@ class Menu extends Component {
         })
 	}
 
+   onClickToggleMenu = (e) => {
+      let c= (this.state.menuToggle===0) ? "menu-page action-slide-out" : "menu-page action-slide-in";
+      this.setState({menuClass: c, menuToggle: 1-this.state.menuToggle});
+   }
+
     render(){
         return (
-            <div className="menu-page">
+            <div className={this.state.menuClass}>
+               <div className="menu-button" onClick={this.onClickToggleMenu}>&lt;</div>
                 <CountryCodes countryCode={this.state.countryCode} handleChange={event => this.handleCountryCodeChange(event)} addHolidays={this.addHolidays} />
+                <div className="menu-spacer"></div>
+                <a className="menu-topitem" href="#"><div className="menu-item" id="1">Item 1</div></a>
+                <a className="menu-topitem" href="#"><div className="menu-item" id="2">Item 2</div></a>
+                <a className="menu-topitem" href="#"><div className="menu-item" id="3">Item 3</div></a>
+                <div className="menu-spacer"></div>
             </div>
         );
     }
