712b724f94ac4c2d981c5ab926fc4219515aeccc
adamredwoods <adamredwoods@users.noreply.github.com>
2018-02-04 12:02:11 -0800

    css changes in header and menu

--- client/src/App.js
+++ client/src/App.js
@@ -11,13 +11,7 @@ import Profile from './Profile.js';
 import Signup from './auth/Signup.js';
 
 
-const Title = (props) => {
-   return (
-      <div>
-         <div className="username-title">{props.userName}</div><div className="calendar-title">{props.calendarTitle}</div>
-      </div>
-   )
-}
+
 
 class App extends Component {
   constructor(props){
@@ -105,7 +99,7 @@ class App extends Component {
             <Header user={this.state.user} calendar={this.state.calendar} onClickEventAction={this.onClickEventAction}/>
             <div className="space">
               <Flash flashType={this.state.flashType} flash={this.state.flash} setFlash={this.setFlash} cancelFlash={this.cancelFlash} />
-              <Title userName={this.state.user.name} calendarTitle={this.state.calendar.name} />
+
               <Route exact path="/" component={() => (<Main calendar={this.state.calendar} user={this.state.user} eventAction={this.state.eventAction} eventObject={this.state.eventObject} onClickEventAction={this.onClickEventAction} />)} />
               <Route path="/login" component={
                 () => (<Login calendar={this.state.calendar} user={this.state.user} setFlash={this.setFlash} updateUser={this.updateUser} />)} />
--- client/src/layout/Header.js
+++ client/src/layout/Header.js
@@ -4,6 +4,14 @@ import { Link } from 'react-router-dom';
 import { Row, Col } from 'react-grid-system';
 import Menu from '../Menu.js';
 
+const Title = (props) => {
+   return (
+      <div>
+         <div className="username-title">{props.userName}</div><div className="calendar-title">{props.calendarTitle}</div>
+      </div>
+   )
+}
+
 class Header extends Component {
    constructor(props) {
       super(props);
@@ -20,15 +28,22 @@ class Header extends Component {
   render(){
     let links = <span />
     if(this.props.user){
-      links = <span><a href="#" onClick={this.onClickToggleMenu}>menu</a><Link className="app-logo" to="/">ProlifiCal</Link><Link to="/profile">Profile</Link><Logout /></span>;
+      links = <Row>
+         <Col sm={5}><Title userName={this.props.user.name} calendarTitle={this.props.calendar.name} /></Col>
+         <Col sm={7}>
+         <a href="#" onClick={this.onClickToggleMenu}>Menu</a>
+         <Link className="app-logo" to="/">ProlifiCal</Link><Link to="/profile">Profile</Link><Logout />
+         </Col>
+      </Row>;
     }
     else {
-      links = <span><Link className="app-logo" to="/">ProlifiCal</Link><Link to="/login">Login</Link><Link to="/signup">Sign Up</Link></span>;
+      links = <span><Link className="app-logo" to="/">Calendar</Link><Link to="/login">Login</Link><Link to="/signup">Sign Up</Link></span>;
     }
     return(
-        <div><div className="nav">
-          {links}
-        </div>
+        <div>
+         <div className="nav">
+            {links}
+          </div>
         <Menu calendar={this.props.calendar} user={this.props.user} onClickToggleMenu={this.onClickToggleMenu} showMenu={this.state.showMenu} onClickEventAction={this.props.onClickEventAction}/>
         </div>
       );
