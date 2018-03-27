8c36f3eaa2ec5603f6d99a37ef01885efdd72ab5
adamredwoods <adamredwoods@users.noreply.github.com>
2018-03-27 14:44:57 -0700

    console debugging

--- client/src/Main.js
+++ client/src/Main.js
@@ -172,7 +172,7 @@ class Main extends Component {
 			user: currentUser,
 			calendar: currentCalendar
 		})).then(response => {
-			//console.log(response.data);
+			console.log("ADD EVENT:",response.data);
 
 			//-- update the calendar state
 			let c = base.state.calendarEvents;
